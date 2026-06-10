7cd60cf4caf76adfcbf35f7f05bfb6c583e362e7
Ubuntu <sanchithahewagamage@gmail.com>
2026-06-10 11:48:33 +0000

    fix: cargo fmt

--- src/protocols/raw_socket.rs
+++ src/protocols/raw_socket.rs
@@ -292,7 +292,8 @@ impl RawSocket {
             _ => return Err(anyhow!("Failed to create raw socket")),
         };
 
-        let _ = tx.send_to(packet, None)
+        let _ = tx
+            .send_to(packet, None)
             .ok_or_else(|| anyhow!("Failed to send packet"))?;
 
         Ok(())
